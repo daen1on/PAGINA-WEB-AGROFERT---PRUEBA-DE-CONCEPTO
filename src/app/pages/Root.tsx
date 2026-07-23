c038348c7b8dd0f89be48789945edd2a6b942b50
NullVoiceDev <stiiv55@gmail.com>
2026-07-22 20:18:00 -0500

    se agrega la funcion scroll to top en el root para que los enlaces todos lleven directo a la parte superior de la web

--- src/app/pages/Root.tsx
+++ src/app/pages/Root.tsx
@@ -2,14 +2,19 @@ import { Outlet } from "react-router";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { WhatsAppButton } from "../components/WhatsAppButton";
+import { ScrollToTop } from "../components/ScrollToTop";
 
 export default function Root() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
+
       <Navbar />
+
       <main className="flex-1">
         <Outlet />
       </main>
+
       <Footer />
       <WhatsAppButton />
     </div>
